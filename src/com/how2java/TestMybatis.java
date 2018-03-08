71887fa599a15b1cf8f02a2775322b509cf50b60
Benjamin Dong <benjamin.dong@sap.com>
2018-03-08 10:07:40 +0800

    multi to multi

--- src/com/how2java/TestMybatis.java
+++ src/com/how2java/TestMybatis.java
@@ -12,28 +12,46 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import com.how2java.pojo.Category;
+import com.how2java.pojo.Order;
+import com.how2java.pojo.OrderItem;
 import com.how2java.pojo.Product;
 
 public class TestMybatis {
-	
+
 	public static void main(String[] args) throws IOException {
-        String resource = "mybatis-config.xml";
-        InputStream inputStream = Resources.getResourceAsStream(resource);
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        SqlSession session=sqlSessionFactory.openSession();
-        
-        List<Product> ps = session.selectList("listProduct");
-        for (Product p : ps) {
-            System.out.println(p + "对应的分类是\t" + p.getCategory());
-        }  
-        
-        session.commit();
-        session.close();
-    }
-	 private static void listAll(SqlSession session) {
-	        List<Category> cs = session.selectList("listCategory");
-	        for (Category c : cs) {
-	            System.out.println(c.getName());
-	        }
-	    }
+		String resource = "mybatis-config.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		SqlSession session = sqlSessionFactory.openSession();
+		
+		addOrderItem(session);
+		listOrder(session);
+
+		session.commit();
+		session.close();
+	}
+
+	private static void listOrder(SqlSession session) {
+		List<Order> os = session.selectList("listOrder");
+		for (Order o : os) {
+			System.out.println(o.getCode());
+			List<OrderItem> ois = o.getOrderItems();
+			for (OrderItem oi : ois) {
+				System.out.format("\t%s\t%f\t%d%n", oi.getProduct().getName(), oi.getProduct().getPrice(),
+						oi.getNumber());
+			}
+		}
+	}
+	
+	private static void addOrderItem(SqlSession session){
+		
+		Order o1 = session.selectOne("getOrder", 1);
+		Product p6 = session.selectOne("getProduct", 6);
+		OrderItem oi = new OrderItem();
+		oi.setProduct(p6);
+		oi.setOrder(o1);
+		oi.setNumber(200);
+		
+		session.insert("addOrderItem", oi);
+	}
 }
